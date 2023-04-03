d2884b4ddef0a5f6bc487ebb7227d9108d286959
ali asgari <aamining@gmail.com>
2023-04-03 19:11:10 +1000

    try to make it on production 19

--- src/application.ts
+++ src/application.ts
@@ -30,7 +30,7 @@ export class LoopbackApplication extends BootMixin(
     this.sequence(MySequence);
 
     // Set up default home page
-    this.static('https://reandcomm.herokuapp.com', path.join(__dirname, '../client/public/index.html'));
+    this.static('/', path.join(__dirname, '../client/public/index.html'));
 
     // Customize @loopback/rest-explorer configuration here
     this.configure(RestExplorerBindings.COMPONENT).to({
